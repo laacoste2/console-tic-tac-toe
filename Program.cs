cdee2fc04dfd8f4c79701a3918c682f330ec0063
Rafael Rodrigues <rafaelrodriguesmirandadiniz@gmail.com>
2024-02-23 18:41:04 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,146 @@
+﻿
+using System.Security.Cryptography;
+
+namespace C__Player_s_Guide___Tic_Tac_Toe 
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randomNumber = rand.Next(1, 3);
+
+            int rows = 3;
+            int columns = 3;
+
+            string[,] table = new string[rows, columns];
+
+            table[0, 0] = "-";
+            table[0, 1] = "-";
+            table[0, 2] = "-";
+            table[1, 0] = "-";
+            table[1, 1] = "-";
+            table[1, 2] = "-";
+            table[2, 0] = "-";
+            table[2, 1] = "-";
+            table[2, 2] = "-";
+
+            while (true)
+            {
+                Square.VerifyIfWinOrDraw(table);
+                Console.Clear();
+                Square.PrintMatrix(table);
+
+                try
+                {
+                    Console.WriteLine("Player 1 (X) - Type the position: ");
+                    int positionP1 = int.Parse(Console.ReadLine());
+                    Square.VerifyPosition(positionP1);
+
+                    if (positionP1 == 1)
+                    {
+                        table[2, 0] = "X";
+
+                    }
+                    else if (positionP1 == 2)
+                    {
+                        table[2, 1] = "X";
+                    }
+                    else if (positionP1 == 3)
+                    {
+                        table[2, 2] = "X";
+                    }
+                    else if (positionP1 == 4)
+                    {
+                        table[1, 0] = "X";
+                    }
+                    else if (positionP1 == 5)
+                    {
+                        table[1, 1] = "X";
+                    }
+                    else if (positionP1 == 6)
+                    {
+                        table[1, 2] = "X";
+                    }
+                    else if (positionP1 == 7)
+                    {
+                        table[0, 0] = "X";
+                    }
+                    else if (positionP1 == 8)
+                    {
+                        table[0, 1] = "X";
+                    }
+                    else if (positionP1 == 9)
+                    {
+                        table[0, 2] = "X";
+                    }
+                }
+                catch(System.FormatException e) 
+                { 
+                    Console.WriteLine("Invalid Type! Write a number");
+                    Console.ReadLine();
+                }
+
+                Square.VerifyIfWinOrDraw(table);
+
+                Console.Clear();
+                Square.PrintMatrix(table);
+
+
+                try
+                {
+                    Console.WriteLine("Player 2 (O) - Type the position: ");
+                    int positionP2 = int.Parse(Console.ReadLine());
+                    Square.VerifyPosition(positionP2);
+
+                    if (positionP2 == 1)
+                    {
+                        table[2, 0] = "O";
+
+                    }
+                    else if (positionP2 == 2)
+                    {
+                        table[2, 1] = "O";
+                    }
+                    else if (positionP2 == 3)
+                    {
+                        table[2, 2] = "O";
+                    }
+                    else if (positionP2 == 4)
+                    {
+                        table[1, 0] = "O";
+                    }
+                    else if (positionP2 == 5)
+                    {
+                        table[1, 1] = "O";
+                    }
+                    else if (positionP2 == 6)
+                    {
+                        table[1, 2] = "O";
+                    }
+                    else if (positionP2 == 7)
+                    {
+                        table[0, 0] = "O";
+                    }
+                    else if (positionP2 == 8)
+                    {
+                        table[0, 1] = "O";
+                    }
+                    else if (positionP2 == 9)
+                    {
+                        table[0, 2] = "O";
+                    }
+                }catch (System.FormatException e)
+                {
+                    Console.WriteLine("Invalid Type! Write a number");
+                    Console.ReadLine();
+                }
+
+                Console.Clear();
+                Square.PrintMatrix(table);
+            }
+
+        }
+
+    }     
+}
---
+++ Square.cs
@@ -0,0 +1,180 @@
+﻿
+namespace C__Player_s_Guide___Tic_Tac_Toe
+{
+    internal class Square
+    {
+
+        public static void VerifyIfWinOrDraw(string[,] matrix)
+        {
+            //O
+            if (matrix[2, 0] == "O" && matrix[2, 1] == "O" && matrix[2, 2] == "O") //1 2 3
+            {
+                Console.Clear();
+                PrintMatrix(matrix);
+                Console.WriteLine("The player using the 'O' WON!!");
+                Console.ReadLine();
+                Environment.Exit(0);
+
+            }
+            else if (matrix[1, 0] == "O" && matrix[1, 1] == "O" && matrix[1, 2] == "O") //4 5 6
+            {
+                Console.Clear();
+                PrintMatrix(matrix);
+                Console.WriteLine("The player using the 'O' WON!!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else if (matrix[0, 0] == "O" && matrix[0, 1] == "O" && matrix[0, 2] == "O") //7 8 9
+            {
+                Console.Clear();
+                PrintMatrix(matrix);
+                Console.WriteLine("The player using the 'O' WON!!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else if (matrix[2, 0] == "O" && matrix[1, 1] == "O" && matrix[0, 2] == "O") //1 5 9
+            {
+                Console.Clear();
+                PrintMatrix(matrix);
+                Console.WriteLine("The player using the 'O' WON!!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else if (matrix[2, 2] == "O" && matrix[1, 1] == "O" && matrix[0, 0] == "O") //3 5 7
+            {
+                Console.Clear();
+                PrintMatrix(matrix);
+                Console.WriteLine("The player using the 'O' WON!!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else if (matrix[2, 0] == "O" && matrix[1, 0] == "O" && matrix[0, 0] == "O") //1 4 7
+            {
+                Console.Clear();
+                PrintMatrix(matrix);
+                Console.WriteLine("The player using the 'O' WON!!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else if (matrix[2, 2] == "O" && matrix[1, 2] == "O" && matrix[0, 2] == "O") //3 6 9
+            {
+                Console.Clear();
+                PrintMatrix(matrix);
+                Console.WriteLine("The player using the 'O' WON!!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else if (matrix[2, 1] == "O" && matrix[1, 1] == "O" && matrix[0, 1] == "O") //2 5 8
+            {
+                Console.Clear();
+                PrintMatrix(matrix);
+                Console.WriteLine("The player using the 'O' WON!!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            //X 
+            if (matrix[2, 0] == "X" && matrix[2, 1] == "X" && matrix[2, 2] == "X") //1 2 3
+            {
+                Console.Clear();
+                PrintMatrix(matrix);
+                Console.WriteLine("The player using the 'X' WON!!");
+                Console.ReadLine();
+                Environment.Exit(0);
+
+            }
+            else if (matrix[1, 0] == "X" && matrix[1, 1] == "X" && matrix[1, 2] == "X") //4 5 6
+            {
+                Console.Clear();
+                PrintMatrix(matrix);
+                Console.WriteLine("The player using the 'X' WON!!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else if (matrix[0, 0] == "X" && matrix[0, 1] == "X" && matrix[0, 2] == "X") //7 8 9
+            {
+                Console.Clear();
+                PrintMatrix(matrix);
+                Console.WriteLine("The player using the 'X' WON!!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else if (matrix[2, 0] == "X" && matrix[1, 1] == "X" && matrix[0, 2] == "X") //1 5 9
+            {
+                Console.Clear();
+                PrintMatrix(matrix);
+                Console.WriteLine("The player using the 'X' WON!!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else if (matrix[2, 2] == "X" && matrix[1, 1] == "X" && matrix[0, 0] == "X") //3 5 7
+            {
+                Console.Clear();
+                PrintMatrix(matrix);
+                Console.WriteLine("The player using the 'X' WON!!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else if (matrix[2, 0] == "X" && matrix[1, 0] == "X" && matrix[0, 0] == "X") //1 4 7
+            {
+                Console.Clear();
+                PrintMatrix(matrix);
+                Console.WriteLine("The player using the 'X' WON!!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else if (matrix[2, 2] == "X" && matrix[1, 2] == "X" && matrix[0, 2] == "X") //3 6 9
+            {
+                Console.Clear();
+                PrintMatrix(matrix);
+                Console.WriteLine("The player using the 'X' WON!!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else if (matrix[2, 1] == "X" && matrix[1, 1] == "X" && matrix[0, 1] == "X") //2 5 8
+            {
+                Console.Clear();
+                PrintMatrix(matrix);
+                Console.WriteLine("The player using the 'X' WON!!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+        }
+
+        public static void PrintMatrix(string[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+
+            }
+        }
+
+        public static void VerifyPosition(int position)
+        {
+            try
+            {
+                if (position < 1 | position > 9)
+                {
+                    throw new IndexOutOfRangeException("\nInvalid Position! The avaiable positions is: 1 to 9");
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+                return;
+            }
+        }
+
+        //Terminar depois
+        
+    }
+}
